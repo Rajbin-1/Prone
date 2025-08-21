5189dfe7be8bd541e07826fd8c1af01aea36f457
Rajbin-1 <rajbinkarki22@gmail.com>
2025-08-21 19:10:25 +0545

    Update app.py

--- app.py
+++ app.py
@@ -317,6 +317,7 @@ def index():
     )
 
 if __name__ == "__main__":
-    # For production use a proper WSGI server; debug True is useful during development
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # default 5000 locally
+    app.run(host="0.0.0.0", port=port)
+
 
